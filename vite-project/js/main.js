ad40c992def19d61097a45a2a86b1586abd2ef45
SpinlyBinly <144476929+SpinlyBinly@users.noreply.github.com>
2023-11-10 11:43:53 -0500

    commit 2

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -3,3 +3,23 @@ import "./style.css";
 //input type check box
 //event listener - filter array (2 items)
 //another checkbox to change theme (light and dark)
+
+const DOMSelectors = {
+  formID: document.querySelector("#formID"),
+  bookTitle: document.querySelector("#bookTitle"),
+  bookAuthor: document.querySelector("#bookAuthor"),
+  bookCover: document.querySelector("#bookCover"),
+  h2s: document.querySelectorAll("h2"),
+  container: document.querySelector(".container"),
+  button: document.querySelector(".btn"),
+};
+
+function makeCard() {
+  DOMSelectors.container.insertAdjacentHTML(
+    "beforeend",
+    `<div class=card>
+      <h2>Book Title: ${bookTitle.value} </h2>
+      <h2> Book Author: ${bookAuthor.value} </h2>  
+      <img src="${bookCover.value}" alt="">`
+  );
+}
