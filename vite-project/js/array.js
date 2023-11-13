c4e692698ac501e37aa84e450ca0dd2c5084640a
SpinlyBinly <144476929+SpinlyBinly@users.noreply.github.com>
2023-11-13 13:07:40 -0500

    commit 5

--- vite-project/js/array.js
+++ vite-project/js/array.js
@@ -2,61 +2,120 @@ const opps = [
   {
     firstName: "Muhammad",
     lastName: "Ali",
-    abilities: ["boxing", "strong"],
+    abilities:[
+      "boxing", 
+      "strong"],
+    weaknesses:[
+      "Poor Defense", 
+      "flying", 
+      "heights"],
     win: true,
   },
+
   {
     firstName: "T-rex",
     lastName: "from Jurassic world",
-    abilities: ["teeth", "bite", "scary"],
+    abilities:[
+      "teeth", 
+      "bite", 
+      "scary"],
+    weaknesses:[
+      "short arms"
+    ],
     win: true,
   },
+
   {
     firstName: "Oleg",
     lastName: "Ponyatyshyn",
-    abilities: ["master coder", "volleyball", ""],
+    abilities: [
+      "master coder", 
+      "volleyball", 
+      ""
+    ],
+    weaknesses:[
+      "heritage russian", 
+      "Mrs. Levina", 
+     "Mrs. Goyco", 
+     ""
+  ],
     win: false,
   },
+
   {
     firstName: "Muhammad",
     lastName: "Ali's dog",
-    abilities: ["teeth", "bite", "scary"],
+    abilities: 
+    [
+      "teeth", "bite", "scary"
+    ],
+    weaknesses:,
     win: false,
   },
+
   {
     firstName: "Peely",
     lastName: "from Fortnite",
-    abilities: ["Fortnite Battlepass"],
+    abilities: 
+    [
+      "Fortnite Battlepass"
+    ],
+    weaknesses:,
     win: false,
   },
+
   {
     firstName: "Genghis",
     lastName: "Khan",
-    abilities: ["red", "yellow", "black"],
+    abilities: 
+    [
+      "Strong", 
+      "Organization",
+      "Mobility", 
+      "Ruthlessness", 
+      "Purpose", 
+      "Determination",  
+      "babymaker3000",
+      "chick magnet",
+      "chaRIZZma"  
+    ],
+    weaknesses:,
     win: true,
   },
+
   {
     firstName: "Fnord",
     lastName: "Peggleblast",
     abilities: ["Supirior Guide", "mlp reject"],
+    weaknesses:["ginger"],
     win: false,
   },
+
   {
     firstName: "Chief",
     lastName: "the Cow",
-    abilities: ["babymaker3000", "chick magnet", "chaRIZZma"],
+    abilities: [
+      "babymaker3000",
+      "chick magnet",
+      "chaRIZZma"
+    ],
+    weaknesses:[""],
     win: true,
   },
+
   {
     firstName: "Master",
     lastName: "Oogway",
-    abilities: ["Kung Fu"],
+    abilities: ["Kung Fu", "Chemistry"],
+    weaknesses:,
     win: true,
   },
+
   {
     firstName: "Kratos",
     lastName: "God of War",
-    abilities: [
+    abilities: 
+    [
       "Stamina",
       "Speed",
       "Healing",
@@ -70,18 +129,60 @@ const opps = [
       "Superhuman agility",
       "Time Manipulation",
     ],
+    weaknesses:
+    [
+      "Illusions", 
+      "Nightmares", 
+      "too angy"
+    ],
     win: true,
   },
+
   {
     firstName: "Figment",
     lastName: "of imagination",
-    abilities: ["purple"],
-    win: false,
-  },
+    abilities: 
+    [
+      "purple"
+    ],
+    weaknesses:["purple"],
+    },
+
   {
     firstName: "Terrance",
     lastName: "Angry Birds",
-    abilities: ["round", "big", "red"],
-    win: true,
+    abilities: 
+    [
+      "round", 
+      "big", 
+      "red"
+    ],
+    weaknesses:["ginger"],
   },
+
+  {
+    firstName: "Red",
+    lastName: "m&m",
+    abilities: 
+    [
+      "round", 
+      "big", 
+      "red"
+    ],
+    weaknesses:["ginger"],
+  },
+
+  {
+    firstName: "Yosemite",
+    lastName: "Sam",
+    abilities: 
+    [
+      "round", 
+      "big", 
+      "red",
+      "guns",
+    ],
+    weaknesses:["ginger"],
+  },
+
 ];
