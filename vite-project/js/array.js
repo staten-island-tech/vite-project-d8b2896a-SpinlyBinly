db7a0a5642f3388b1b4620eaac11fab71aaf1568
SpinlyBinly <144476929+SpinlyBinly@users.noreply.github.com>
2023-11-14 13:14:06 -0500

    commit 6

--- vite-project/js/array.js
+++ vite-project/js/array.js
@@ -2,120 +2,89 @@ const opps = [
   {
     firstName: "Muhammad",
     lastName: "Ali",
-    abilities:[
-      "boxing", 
-      "strong"],
-    weaknesses:[
-      "Poor Defense", 
-      "flying", 
-      "heights"],
-    win: true,
+    abilities: ["boxing", "strong"],
+    weaknesses: ["Poor Defense", "flying", "heights"],
+    janeWin: true,
   },
 
   {
     firstName: "T-rex",
     lastName: "from Jurassic world",
-    abilities:[
-      "teeth", 
-      "bite", 
-      "scary"],
-    weaknesses:[
-      "short arms"
-    ],
-    win: true,
+    abilities: ["teeth", "bite", "scary"],
+    weaknesses: ["short arms"],
+    janeWin: false,
   },
 
   {
     firstName: "Oleg",
     lastName: "Ponyatyshyn",
-    abilities: [
-      "master coder", 
-      "volleyball", 
-      ""
-    ],
-    weaknesses:[
-      "heritage russian", 
-      "Mrs. Levina", 
-     "Mrs. Goyco", 
-     ""
-  ],
-    win: false,
+    abilities: ["master coder", "volleyball", "short arms"],
+    weaknesses: ["heritage russian", "mrs. levina"],
+    janeWin: true,
   },
 
   {
     firstName: "Muhammad",
     lastName: "Ali's dog",
-    abilities: 
-    [
-      "teeth", "bite", "scary"
-    ],
-    weaknesses:,
-    win: false,
+    abilities: ["teeth", "bite", "scary"],
+    weaknesses: [],
+    janeWin: false,
   },
 
   {
     firstName: "Peely",
     lastName: "from Fortnite",
-    abilities: 
-    [
-      "Fortnite Battlepass"
-    ],
-    weaknesses:,
-    win: false,
+    abilities: ["Fortnite Battlepass"],
+    weaknesses: [""],
+    janeWin: false,
   },
 
   {
     firstName: "Genghis",
     lastName: "Khan",
-    abilities: 
-    [
-      "Strong", 
+    abilities: [
+      "Strong",
       "Organization",
-      "Mobility", 
-      "Ruthlessness", 
-      "Purpose", 
-      "Determination",  
+      "Mobility",
+      "Ruthlessness",
+      "Purpose",
+      "Determination",
       "babymaker3000",
       "chick magnet",
-      "chaRIZZma"  
+      "chaRIZZma",
     ],
-    weaknesses:,
-    win: true,
+    weaknesses: [],
+    janeWin: false,
   },
 
   {
     firstName: "Fnord",
     lastName: "Peggleblast",
     abilities: ["Supirior Guide", "mlp reject"],
-    weaknesses:["ginger"],
-    win: false,
+    weaknesses: ["ginger"],
+    janeWin: false,
   },
 
   {
     firstName: "Chief",
     lastName: "the Cow",
-    abilities: [
-      "babymaker3000",
-      "chick magnet",
-      "chaRIZZma"
-    ],
-    weaknesses:[""],
-    win: true,
+    abilities: ["babymaker3000", "chick magnet", "chaRIZZma"],
+    weaknesses: [""],
+    janeWin: false,
   },
 
   {
     firstName: "Master",
     lastName: "Oogway",
     abilities: ["Kung Fu", "Chemistry"],
-    weaknesses:,
-    win: true,
+    weaknesses: [],
+    janeWin: false,
   },
 
   {
     firstName: "Kratos",
     lastName: "God of War",
-    abilities: 
-    [
+    abilities: [
       "Stamina",
       "Speed",
       "Healing",
@@ -129,60 +98,39 @@ const opps = [
       "Superhuman agility",
       "Time Manipulation",
     ],
-    weaknesses:
-    [
-      "Illusions", 
-      "Nightmares", 
-      "too angy"
-    ],
-    win: true,
+    weaknesses: ["Illusions", "Nightmares", "too angy"],
+    janeWin: false,
   },
 
   {
     firstName: "Figment",
     lastName: "of imagination",
-    abilities: 
-    [
-      "purple"
-    ],
-    weaknesses:["purple"],
-    },
+    abilities: ["purple"],
+    weaknesses: ["purple"],
+    janeWin: true,
+  },
 
   {
     firstName: "Terrance",
     lastName: "Angry Birds",
-    abilities: 
-    [
-      "round", 
-      "big", 
-      "red"
-    ],
-    weaknesses:["ginger"],
+    abilities: ["round", "big", "red"],
+    weaknesses: ["ginger"],
+    janeWin: false,
   },
 
   {
     firstName: "Red",
     lastName: "m&m",
-    abilities: 
-    [
-      "round", 
-      "big", 
-      "red"
-    ],
-    weaknesses:["ginger"],
+    abilities: ["round", "big", "red"],
+    weaknesses: ["ginger"],
+    janeWin: true,
   },
 
   {
     firstName: "Yosemite",
     lastName: "Sam",
-    abilities: 
-    [
-      "round", 
-      "big", 
-      "red",
-      "guns",
-    ],
-    weaknesses:["ginger"],
+    abilities: ["round", "big", "red", "guns"],
+    weaknesses: ["ginger"],
+    janeWin: false,
   },
-
 ];
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -7,21 +7,44 @@ console.log(opps[0] + " is a major opp, 100% facts");
 //another checkbox to change theme (light and dark)
 
 const DOMSelectors = {
-  formID: document.querySelector("#formID"),
-  bookTitle: document.querySelector("#bookTitle"),
-  bookAuthor: document.querySelector("#bookAuthor"),
-  bookCover: document.querySelector("#bookCover"),
+  container: document.querySelector(".container"),
+  lightButton: document.querySelector("#lightButton"),
+  darkButton: document.querySelector("#darkButton"),
+  winButton: document.querySelector("#winButton"),
+  loseButton: document.querySelector("#loseButton"),
+  tieButton: document.querySelector("#tieButton"),
   h2s: document.querySelectorAll("h2"),
   container: document.querySelector(".container"),
   button: document.querySelector(".btn"),
 };
 
+function clearFields() {
+  DOMSelectors.___.value = "";
+  DOMSelectors.___.value = "";
+  DOMSelectors.___.value = "";
+}
+
 function makeCard() {
   DOMSelectors.container.insertAdjacentHTML(
     "beforeend",
     `<div class=card>
-      <h2>Book Title: ${bookTitle.value} </h2>
+      <h2> ${firstName.value} ${lastname.value} </h2>
       <h2> Book Author: ${bookAuthor.value} </h2>  
       <img src="${bookCover.value}" alt="">`
   );
 }
+
+DOMSelectors.formID.addEventListener("submit", function (event) {
+  event.preventDefault();
+  makeCard();
+  clearFields();
+  function remove() {
+    let buttons = document.querySelectorAll("button");
+    buttons.forEach((btn) => {
+      btn.addEventListener("click", function (event) {
+        event.currentTarget.parentElement.remove();
+      });
+    });
+  }
+  remove();
+});
