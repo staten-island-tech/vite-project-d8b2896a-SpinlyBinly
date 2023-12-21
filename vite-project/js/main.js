62c835d63044a3116dcdc9af9fc92d4f8c20f265
SpinlyBinly <144476929+SpinlyBinly@users.noreply.github.com>
2023-12-21 12:12:07 -0500

    commit 29

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -32,9 +32,9 @@ populate(opps);
 function filters() {
   DOMSelectors.buttons.forEach((btn) =>
     btn.addEventListener("click", function () {
-      let category = btn.textContent.toLowerCase()
+      let category = btn.textContent.toLowerCase();
       let newArr = opps.filter((el) => el.janeWin.includes(category));
-      clearFields()
+      clearFields();
       populate(newArr);
     })
   );
@@ -44,12 +44,12 @@ filters();
 document.querySelector("#themeBTN").addEventListener("click", function () {
   if (document.body.classList.contains("light")) {
     document.body.classList.add("dark");
-    document.body.classList.remove("light");       
+    document.body.classList.remove("light");
   } else {
     document.body.classList.remove("dark");
     document.body.classList.add("light");
   }
-  populate(opps)
+  populate(opps);
 });
 
 /* DOMSelectors.winButton.addEventListener("click", function () {
